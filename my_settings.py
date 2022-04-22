777b8006a24f94a98522385df18e7e5e8e259acd
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-22 15:06:39 +0900

    project initial setting

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jistagram',
+        'USER': 'root',
+        'PASSWORD': '0206',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-8v8yqb!g$t8#br1o7j+15rb^2c^ry#5e_tjr#)5g@_6sy9p_n#'
